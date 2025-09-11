a1e5aee5b245eb7e337ec8e7e52947f27ceba6cf
ramir.alves.melo <ramir.alves.melo@gmail.com>
2025-09-11 11:48:58 -0300

    OK

--- src/main/java/br/com/ramiralvesmelo/audit/document/AuditLogDoc.java
+++ src/main/java/br/com/ramiralvesmelo/audit/document/AuditLogDoc.java
@@ -10,13 +10,7 @@ import lombok.Data;
 @Data
 @Document(collection = "audit_log_doc")
 public class AuditLogDoc {
-
     @Id
     private String id;
     private Map<String, Object> payload;
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    public Map<String, Object> getPayload() { return payload; }
-    public void setPayload(Map<String, Object> payload) { this.payload = payload; }
-
 }
